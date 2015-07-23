2f799e1fceb5870b885627d2c3a9f166e0cefd2a
Raymond E Cochrane <raymondecochrane@gmail.com>
2015-07-23 22:09:50 +0100

    completed

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, uniqueness: { case_sensitive: false }
+end
